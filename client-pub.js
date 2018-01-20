e375f5bd677e98f0f597cfdcdc443c3abb2e6fd6
Parik <parikshitd92@gmail.com>
2018-01-20 15:57:42 -0500

    chrome extension early file

--- client-pub.js
+++ client-pub.js
@@ -1,20 +1,22 @@
 var mqtt    = require('mqtt');
 //var pub  = mqtt.connect('tcp://0.0.0.0:61620',{encoding:'utf8', clientId: 'Publishers'});
-var pub  = mqtt.connect('mqtt://127.0.0.1:1883',{encoding:'utf8', clientId: 'jayantjnp@gmail.com'});
+var pub  = mqtt.connect('mqtt://10.103.226.26:1883',{encoding:'utf8', clientId: 'jayantjnp@gmail.com'});
  
 //pub.on();
 //pub.publish('esp/12-31-13-AA-FD-43', '12-31-13-AA-FD-433005', {retain:false, qos: 0});
 // pub.publish('register', 'jayants123', {retain:true, qos: 0});
 var jsonS={
-	"username":'rohit@gmail.com',
-	"password":'Rohits',
-	"name" : 'Rohit Nigam',
+	"username":'parik@gmail.com',
+	"password":'parik00',
+	"name" : 'Parikshit',
 	"gender" :  'Male',
-	"contact" : 9044775913
+	"contact" : 123456789
 };
 // var jsonS={
-// 	"username":'jayantjnp@gmail.com',//host
+// 	"username":'parik@gmail.com',//host
 // 	"email":'rohit@gmail.com',//for friend
 // };
 pub.publish('connect', JSON.stringify(jsonS), {retain:false, qos: 0});
 pub.end();
+
+module.exports = function (n) { return n  }
